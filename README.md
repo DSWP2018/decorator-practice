fd25d90e5c13b8c4f8b35dd209163604bcfcbc8e
Rodrigo Villalobos <rvillalobos@tkgdl-211.tacitknowledge.com>
2018-03-12 16:40:55 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Membership App
+=======
+
+Decorator pattern practice
+
+- Transform the code to use decorator pattern to decorate a membership with different bennefits
+- Create decorators for each different benefit
+- Modify the code so that each benefit modifies the cost of the membership
+- Make sure that checkstyle do not report any error at the end
+
+
+NOTE: No unit tests are required for this practice
---
+++ src/main/java/com/iteso/decorator/memberships/UltimateMembership.java
@@ -0,0 +1,38 @@
+package com.iteso.decorator.memberships;
+
+import com.iteso.decorator.Membership;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rvillalobos on 4/14/16.
+ */
+public class UltimateMembership extends Membership {
+
+
+    public static final int DOWNLOAD_CAPACITY = 3000;
+    public static final int COST = 1299;
+    public static final int CLOUD_STORAGE = 1000;
+    public static final int ALLOWED_DEVICES = -1;
+    public static final String GOLD_MEMBERSHIP = "Ultimate Membership";
+    public static final int SIMULTANEOUS_LOGINS = 20;
+    public static final boolean SUPPORT = true;
+    public static final String ULTIMATE = "Ultimate";
+    public static final boolean UNLIMITED_DEVICES = true;
+
+    public UltimateMembership() {
+        setAllowedDevices(ALLOWED_DEVICES);
+        setCloudStorage(CLOUD_STORAGE);
+        setCost(COST);
+        setDescription(GOLD_MEMBERSHIP);
+        setDownloadCapacity(DOWNLOAD_CAPACITY);
+        setSimultaneousLogins(SIMULTANEOUS_LOGINS);
+        setSupport(SUPPORT);
+        setSupportCoverage(ULTIMATE);
+        setUnlimitedDevices(UNLIMITED_DEVICES);
+    }
+
+    public ArrayList getAllBenefits() {
+        return null;
+    }
+}
