0c1bb16f9536038cc29bded8c8dbca504ae181ee
Damian Garcia Serrano <damian95.gs@gmail.com>
2018-03-13 03:28:20 -0600

    Corregir estilo del main (whoops)

--- src/main/java/com/iteso/decorator/Main.java
+++ src/main/java/com/iteso/decorator/Main.java
@@ -2,20 +2,27 @@ package com.iteso.decorator;
 
 import com.iteso.decorator.benefits.DownloadCapacityBenefit;
 import com.iteso.decorator.benefits.SimultaneousLoginsBenefit;
-import com.iteso.decorator.benefits.SupportBenefit;
 import com.iteso.decorator.benefits.UnlimitedDevicesBenefit;
 import com.iteso.decorator.memberships.BaseMembership;
 
-public class Main{
-    public static void main(String[] args) {
+/** */
+public final class Main {
+    /** */
+    private Main() {
+
+    }
+
+    /**
+     * @param args args
+     */
+    public static void main(final String[] args) {
         Membership m = new SimultaneousLoginsBenefit(
-            new DownloadCapacityBenefit(
-            new UnlimitedDevicesBenefit(
-            new BaseMembership())));
+                new DownloadCapacityBenefit(
+                    new UnlimitedDevicesBenefit(new BaseMembership())));
 
         System.out.println("Cost" + m.getCost());
 
-        m.getAllBenefits().forEach((k,v) -> {
+        m.getAllBenefits().forEach((k, v) -> {
             System.out.println(k + ":" + v);
         });
     }
